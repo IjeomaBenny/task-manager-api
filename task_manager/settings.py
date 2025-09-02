7a5959d64fdc382d082d78b1066ea889f7a1026c
ijeomaBenny <ijesmart40@gmail.com>
2025-09-02 01:10:29 +0100

    Wire up DRF: Task serializer, viewset, router; auth and browsable API working

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -42,6 +42,17 @@ INSTALLED_APPS = [
 ]
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
